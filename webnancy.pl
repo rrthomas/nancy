b471c366e25300843a4fb77ce60fd4273dcd3796
rrt <rrt@9c2eb239-9e10-0410-8bf4-dfbfc08e7956>
2008-12-07 08:15:37 +0000

    New program that uses weavefile to generate pages for a web site dynamically. Developed for academievocale-paris.org.

---
+++ webnancy.pl
@@ -0,0 +1,45 @@
+#! /usr/bin/perl -Tw
+# Web wrapper for Nancy
+# (c) 2002-2008 Reuben Thomas (rrt@sc3d.org, http://rrt.sc3d.org)
+# Distributed under the GNU General Public License
+
+# FIXME: Remove cgi-bin from PATH
+$ENV{PATH} = '/usr/local/bin:/bin:/usr/bin';
+
+use strict;
+use warnings;
+
+use CGI qw(:standard);
+use CGI::Carp 'fatalsToBrowser';
+use CGI::Util qw(unescape);
+use Encode;
+
+use vars qw($BaseUrl);
+
+# Root of site relative to root of server
+$BaseUrl = "/";
+$DocumentRoot = "/var/www";
+
+# Untaint the given value
+# FIXME: Use CGI::Untaint
+sub untaint {
+  my ($var) = @_;
+  return if !defined($var);
+  $var =~ /^(.*)$/ms;           # get untainted value in $1
+  return $1;
+}
+
+my $page = unescape(url());
+my $base = url(-base => 1);
+$base = untaint($base);
+$page =~ s|^$base$BaseUrl||;
+$page =~ s|^/||;
+$page =~ s|\.html$||;
+
+# Perform the request
+my $headers = {};
+$headers->{-charset} = "utf-8";
+open(IN, "-|", "weavefile.pl", $DocumentRoot, $page, "template.html");
+my $out = do { local $/, <IN> };
+close IN;
+print header($headers) . $out;
