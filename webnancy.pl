18cadd82feb6d6b5d1fbc3d461d1265832d695a8
rrt <rrt@9c2eb239-9e10-0410-8bf4-dfbfc08e7956>
2008-12-07 13:47:46 +0000

    Simplify considerably.

--- webnancy.pl
+++ webnancy.pl
@@ -3,41 +3,21 @@
 # (c) 2002-2008 Reuben Thomas (rrt@sc3d.org, http://rrt.sc3d.org)
 # Distributed under the GNU General Public License
 
-$ENV{PATH} = '/usr/local/bin:/bin:/usr/bin';
-
 use strict;
 use warnings;
 
+use Perl6::Slurp;
 use CGI qw(:standard);
-use CGI::Carp 'fatalsToBrowser';
 use CGI::Util qw(unescape);
 
-use vars qw($BaseUrl);
-
 # Root of site relative to root of server
-$BaseUrl = "/";
+my $BaseUrl = "/";
 # Root of source files
-$DocumentRoot = "/var/www";
+my $DocumentRoot = "/var/www";
 
-# Untaint the given value
-# FIXME: Use CGI::Untaint
-sub untaint {
-  my ($var) = @_;
-  return if !defined($var);
-  $var =~ /^(.*)$/ms;           # get untainted value in $1
-  return $1;
-}
-
-my $page = unescape(url());
-my $base = url(-base => 1);
-$base = untaint($base);
-$page =~ s|^$base$BaseUrl||;
-$page =~ s|^/||;
+# Extract file name from URL
+my $page = (unescape(url(-absolute => 1)) =~ s|^$BaseUrl/?||);
 
 # Perform the request
-my $headers = {};
-$headers->{-charset} = "utf-8";
 open(IN, "-|", "weavefile.pl", $DocumentRoot, $page, "template.html");
-my $out = do { local $/, <IN> };
-close IN;
-print header($headers) . $out;
+print header(-charset => "utf-8") . (slurp \*IN);
