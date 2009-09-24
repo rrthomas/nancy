e8c0d7ed1e0b20ba269c80583fc18790bb97b76b
rrt <rrt@9c2eb239-9e10-0410-8bf4-dfbfc08e7956>
2009-09-24 22:46:56 +0000

    Add support for 404 if a non-existent page is requested.
    Remove unnecessary space after hash-bang.

--- webnancy.pl
+++ webnancy.pl
@@ -1,4 +1,4 @@
-#! /usr/bin/perl -T
+#!/usr/bin/perl -T
 # Web wrapper for Nancy
 # (c) 2002-2009 Reuben Thomas (rrt@sc3d.org, http://rrt.sc3d.org)
 # Distributed under the GNU General Public License
@@ -7,6 +7,7 @@ use strict;
 use warnings;
 
 use Perl6::Slurp;
+use File::Spec::Functions qw(catfile);
 use CGI qw(:standard);
 use CGI::Util qw(unescape);
 
@@ -15,12 +16,15 @@ my $BaseUrl = "/";
 # Root of source files
 my $DocumentRoot = "/var/www";
 
+my $template = "template.html";
+
 # Extract file name from URL
 my $page = unescape(url(-absolute => 1));
 $page =~ s|^$BaseUrl/?||;
 $page =~ s|\.html$||;
 $page = "index" if $page eq "";
+$template = "404.html" if !-d catfile($DocumentRoot, $page);
 
 # Perform the request
-open(IN, "-|", "weavefile.pl", $DocumentRoot, $page, "template.html");
-print header(-charset => "utf-8") . (slurp \*IN);
+open(IN, "-|", "weavefile.pl", $DocumentRoot, $page, $template);
+print header() . (slurp \*IN);
