9f8d32f3e71ef6138d2a3e7e97c948cfe47c3c0a
Reuben Thomas <rrt@sc3d.org>
2021-05-01 21:59:22 +0100

    Add minimal types for PCRE2

--- types/pcre2/index.d.ts
+++ types/pcre2/index.d.ts
@@ -1,1 +1,7 @@
-declare module 'pcre2'
+declare module 'pcre2' {
+  export class PCRE2 {
+    constructor (pattern: string, options: string)
+    replace(string: string, replaceValue: string): string
+    replace(string: string, replacer: function): string
+  }
+}
