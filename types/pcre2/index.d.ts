1e8d84bc096c7d235a10eede890a2b4678d168a4
Reuben Thomas <rrt@sc3d.org>
2021-07-04 11:59:51 +0100

    Remove no-longer-needed PCRE2 type declarations

--- types/pcre2/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'pcre2' {
-  export class PCRE2 {
-    constructor (pattern: string, options: string)
-    replace(string: string, replaceValue: string): string
-    replace(string: string, replacer: function): string
-  }
-}
