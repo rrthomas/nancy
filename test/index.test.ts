99822b4d5b560916f6a8d451a493243af2a952f2
Reuben Thomas <rrt@sc3d.org>
2021-08-03 10:13:00 +0100

    Complete test coverage for CLI
    Abort on finding something not a file or directory when crawling the input
    tree.
    
    Run most of the tests with just the module, not the CLI (fixes TODO).

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,8 +1,9 @@
 import util from 'util'
 import fs from 'fs'
 import path from 'path'
+import net from 'net'
 import execa from 'execa'
-import {directory} from 'tempy'
+import tempy, {directory} from 'tempy'
 import {compareSync, Difference} from 'dir-compare'
 import chai from 'chai'
 import chaiAsPromised from 'chai-as-promised'
@@ -37,20 +38,9 @@ function diffsetDiffsOnly(diffSet: Difference[]): Difference[] {
   return diffSet.filter((diff) => diff.state !== 'equal')
 }
 
-async function nancyTest(inputPath: string, expected: string, buildPath?: string) {
-  let outputDir = directory()
-  let outputObj = path.join(outputDir, 'output')
-  const args = [inputPath]
-  if (buildPath) {
-    args.push(`--path=${buildPath}`)
-  }
-  args.push(outputObj)
-  await runNancy(args)
-  assertFileObjEqual(outputObj, expected)
-  fs.rmdirSync(outputDir, {recursive: true})
-
-  outputDir = directory()
-  outputObj = path.join(outputDir, 'output')
+function nancyTest(inputPath: string, expected: string, buildPath?: string) {
+  const outputDir = directory()
+  const outputObj = path.join(outputDir, 'output')
   const inputDirs = inputPath.split(path.delimiter)
   expand(inputDirs[0], outputObj, buildPath, unionFs(inputDirs))
   assertFileObjEqual(outputObj, expected)
@@ -66,60 +56,128 @@ async function checkLinks(root: string, start: string) {
 }
 
 describe('nancy', function () {
-  // The tests are rather slow, but not likely to hang.
+  // In coverage mode, allow for recompilation.
   this.timeout(10000)
 
   before(function () {
     process.chdir('test')
   })
 
+  // Module tests
+  it('Whole-tree test', async () => {
+    nancyTest('webpage-src', 'webpage-expected')
+    await checkLinks('webpage-expected', 'index.html')
+  })
+
+  it('Part-tree test', async () => {
+    nancyTest('webpage-src', 'webpage-expected/people', 'people')
+    await checkLinks('webpage-expected/people', 'index.html')
+  })
+
+  it('Two-tree test', async () => {
+    nancyTest('mergetrees-src:webpage-src', 'mergetrees-expected')
+    await checkLinks('mergetrees-expected', 'index.html')
+  })
+
+  it('Test nested macro invocations', () => {
+    nancyTest('nested-macro-src', 'nested-macro-expected')
+  })
+
+  it('Failing executable test', () => {
+    try {
+      nancyTest('false.nancy.txt', 'dummy')
+    } catch (error) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      expect(error.message).to.contain('Command failed with exit code 1')
+    }
+  })
+
+  it('Passing executable test', () => {
+    nancyTest('true.nancy.txt', 'true-expected.txt')
+  })
+
+  it('Executable test', () => {
+    nancyTest('page-template-with-date-src', 'page-template-with-date-expected')
+  })
+
+  it('Test that macros aren\'t expanded in Nancy\'s command-line arguments', () => {
+    nancyTest('$path-src', '$path-expected')
+  })
+
+  it('Test that $paste doesn\'t expand macros', () => {
+    nancyTest('paste-src', 'paste-expected')
+  })
+
+  it('Cookbook web site example', async () => {
+    nancyTest('cookbook-example-website-src', 'cookbook-example-website-expected')
+    await checkLinks('cookbook-example-website-expected', 'index/index.html')
+  })
+
+  // CLI tests
   it('--help should produce output', async () => {
     const proc = runNancy(['--help'])
     const {stdout} = await proc
     expect(stdout).to.contain('A simple templating system.')
   })
 
-  it('Whole-tree test', async () => {
-    await nancyTest('webpage-src', 'webpage-expected')
-    await checkLinks('webpage-expected', 'index.html')
+  it('Missing command-line argument should cause an error', async () => {
+    try {
+      await runNancy(['dummy'])
+    } catch (error) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      expect(error.stderr).to.contain('the following arguments are required')
+    }
   })
 
-  it('Part-tree test', async () => {
-    await nancyTest('webpage-src', 'webpage-expected/people', 'people')
-    await checkLinks('webpage-expected/people', 'index.html')
+  it('--foo should cause an error', async () => {
+    try {
+      await runNancy(['--foo', 'a', 'b'])
+    } catch (error) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      expect(error.stderr).to.contain('unrecognized arguments: --foo')
+    }
   })
 
-  it('Two-tree test', async () => {
-    await nancyTest('mergetrees-src:webpage-src', 'mergetrees-expected')
-    await checkLinks('mergetrees-expected', 'index.html')
+  it('Running on a non-existent path should cause an error (DEBUG=yes coverage)', async () => {
+    process.env.DEBUG = 'yes'
+    try {
+      await runNancy(['a', 'b'])
+    } catch (error) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      expect(error.stderr).to.contain('no such file or directory')
+    }
+    delete process.env.DEBUG
   })
 
-  it('Test nested macro invocations', async () => {
-    await nancyTest('nested-macro-src', 'nested-macro-expected')
+  it('Running on a non-existent path should cause an error', async () => {
+    try {
+      await runNancy(['a', 'b'])
+    } catch (error) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      expect(error.stderr).to.contain('no such file or directory')
+    }
   })
 
-  it('Failing executable test', async () => {
-    return assert.isRejected(runNancy(['false.nancy.txt', 'dummy']))
+  it('Running on something not a directory or file should cause an error', async () => {
+    const server = net.createServer()
+    const tempFile = tempy.file()
+    server.listen(tempFile)
+    try {
+      await runNancy([`${tempFile}`, 'dummy'])
+    } catch (error) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      expect(error.stderr).to.contain('is not a directory or file')
+    } finally {
+      server.close()
+    }
   })
 
-  it('Passing executable test', async () => {
-    await nancyTest('true.nancy.txt', 'true-expected.txt')
-  })
-
-  it('Executable test', async () => {
-    await nancyTest('page-template-with-date-src', 'page-template-with-date-expected')
-  })
-
-  it('Test that macros aren\'t expanded in Nancy\'s command-line arguments', async () => {
-    await nancyTest('$path-src', '$path-expected')
-  })
-
-  it('Test that $paste doesn\'t expand macros', async () => {
-    await nancyTest('paste-src', 'paste-expected')
-  })
-
-  it('Cookbook web site example', async () => {
-    await nancyTest('cookbook-example-website-src', 'cookbook-example-website-expected')
-    await checkLinks('cookbook-example-website-expected', 'index/index.html')
+  it('Non-existent --path should cause an error', async () => {
+    try {
+      await runNancy(['--path', 'nonexistent', 'webpage-src', 'dummy'])
+    } catch (error) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      expect(error.stderr).to.contain('no such file or directory')
+    }
   })
 })
