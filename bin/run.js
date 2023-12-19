6d09fbcf0bc93f49fb238b9db3b649610dae947a
Reuben Thomas <rrt@sc3d.org>
2023-12-19 19:14:30 +0200

    Fix run.js to run from anywhere

--- bin/run.js
+++ bin/run.js
@@ -1,3 +1,6 @@
 #!/usr/bin/env -S node --no-warnings
 import path from 'path'
-import(path.join('..', 'lib', 'cli.js'))
+import { fileURLToPath } from 'url'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+import(path.join(__dirname, '..', 'lib', 'cli.js'))
